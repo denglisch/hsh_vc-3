14f1cc353e9d8d0bb6a687dbdf18311296c8c29f
Kilian Dangendorf <kilian.dangendorf@stud.hs-hannover.de>
2021-06-10 11:23:38 +0200

    init

--- Haar.py
+++ Haar.py
@@ -2,11 +2,27 @@
 # team01
 
 import numpy as np
+#TODO: needs return?
 
-class Haar:
-    def normalized_decomposition(self):
-        # d
-        return
+def normalized_decomposition_2d(image_values):
+    # Haar decomposition of a 2D array inplace
+    return
+def normalized_reconstruction_2d(image_values):
+    # Haar reconstruction of a 2D array inplace
+    return
+
+def _normalized_decomposition(coefficient):
+    # Haar decomposition of array inplace
+    return
+def _normalized_decomposition_step(coefficient):
+    # One step of Haar decomposition of array inplace
+    return
+def _normalized_reconstruction(coefficient):
+    # Haar reconstruction of array inplace
+    return
+def _normalized_reconstruction_step(coefficient):
+    # One step of Haar reconstruction of array inplace
+    return
 
 
 
