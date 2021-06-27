db989d2c2edcc5d1d92a8c20ab6d10eb4bce1983
Kilian Dangendorf <kilian.dangendorf@stud.hs-hannover.de>
2021-06-27 15:04:22 +0200

    draggable GUI and syn/an matrices

---
+++ MRAGUI.py
@@ -0,0 +1,152 @@
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider
+from matplotlib.widgets import RadioButtons
+import numpy as np
+import math
+
+
+def render_points_array(ax, control_points_array=None, points_array=None):
+    ax.clear()
+    ax.axis([0.0, 1.0, 0.0, 1.0])
+
+    #radius
+    r_cp=0.0025
+    r_p=0.0025
+    #color
+    c_cp='black'
+    c_p='blue'
+    c_sc='red'
+    c_cc='gray'
+
+    ann_offset = np.array([0.006, 0.003])  # offset for annotations
+
+    if control_points_array is not None:
+        for i, cp in enumerate(control_points_array):
+            disc = "c{}".format(i)
+            ax.add_patch(plt.Circle(cp, radius=r_cp, fc=c_cp, fill=True))
+            ax.annotate(disc, cp + ann_offset)
+
+            if i > 0:
+                from_p = control_points_array[i - 1]
+                to_p = cp
+                ax.plot([from_p[0], to_p[0]], [from_p[1], to_p[1]], 'o', ms=2.0, ls='-', lw=1.0, color=c_cc)
+
+    if points_array is not None:
+        for i,p in enumerate(points_array):
+            ax.add_patch(plt.Circle(p, radius=r_p, fc=c_p, fill=False))
+            if i>0:
+                from_p=points_array[i-1]
+                to_p=p
+                ax.plot([from_p[0], to_p[0]], [from_p[1], to_p[1]], 'o', ms=2.0, ls='-', lw=1.0, color=c_sc)
+    return
+
+def build_plt(curve_points_array, get_new_points_to_draw_for_level):
+    #prepare plot
+    fig, ax = plt.subplots(figsize=[12, 12])
+    #init vis
+    level=0
+
+    #render_points_array(ax, curve_points_array, points_array, level)
+    #Slider (widget example adapted from: https://riptutorial.com/matplotlib/example/23577/interactive-controls-with-matplotlib-widgets)
+    #slider axes
+    slider_ax = plt.axes([0.35, .03, 0.50, 0.02])
+    level_slider = Slider(slider_ax, "Level", 0, 10, valinit=level, valstep=1)
+
+    points_array=curve_points_array
+
+    #defined locally to have all values here
+    def update_vis(val):
+        #get selected step
+        nonlocal level,points_array
+        level=val
+
+
+        #points_array,details=calc_new_points(points_array, level)
+
+        points_array=get_new_points_to_draw_for_level(level)
+        #print(points_array)
+
+        #print("Replot at Level {} on {} control points with {} total points".format(step, count_cp, count_p))
+
+        #rebuild vis on axes
+        render_points_array(ax, curve_points_array, points_array)
+        fig.canvas.draw_idle()
+    update_vis(level)
+
+    # call update function on slider value change
+    level_slider.on_changed(update_vis)
+
+    # this was to define points for initial curve ;)
+    def onclick(event):
+        ix, iy = event.xdata, event.ydata
+        print("[{}, {}],".format(ix,iy))
+    fig.canvas.mpl_connect('button_press_event', onclick)
+
+    #from: https://stackoverflow.com/questions/50439506/dragging-points-in-matplotlib-interactive-plot
+    pind = None  # active point
+    epsilon = 5  # max pixel distance
+    def button_press_callback(event):
+        'whenever a mouse button is pressed'
+        nonlocal pind
+        if event.inaxes is None:
+            return
+        if event.button != 1:
+            return
+        # print(pind)
+        pind = get_ind_under_point(event)
+    def button_release_callback(event):
+        'whenever a mouse button is released'
+        nonlocal pind
+        if event.button != 1:
+            return
+        pind = None
+    def get_ind_under_point(event):
+        'get the index of the vertex under point if within epsilon tolerance'
+
+        # display coords
+        # print('display x is: {0}; display y is: {1}'.format(event.x,event.y))
+        t = ax.transData.inverted()
+        tinv = ax.transData
+        xy = t.transform([event.x, event.y])
+        #print('data x is: {0}; data y is: {1}'.format(xy[0],xy[1]))
+        xr = np.reshape(points_array[:,0], (np.shape(points_array[:,0])[0], 1))
+        yr = np.reshape(points_array[:,1], (np.shape(points_array[:,1])[0], 1))
+        xy_vals = np.append(xr, yr, 1)
+        xyt = tinv.transform(xy_vals)
+        xt, yt = xyt[:, 0], xyt[:, 1]
+        d = np.hypot(xt - event.x, yt - event.y)
+        indseq, = np.nonzero(d == d.min())
+        ind = indseq[0]
+
+        # print(d[ind])
+        if d[ind] >= epsilon:
+            ind = None
+        #print(ind)
+        return ind
+
+    def motion_notify_callback(event):
+        'on mouse movement'
+        nonlocal points_array
+        if pind is None:
+            return
+        if event.inaxes is None:
+            return
+        if event.button != 1:
+            return
+        # update yvals
+        # print('motion x: {0}; y: {1}'.format(event.xdata,event.ydata))
+        points_array[pind] = [event.xdata,event.ydata]
+
+        # update curve via sliders and draw
+        #update_vis(level)
+        render_points_array(ax, curve_points_array, points_array)
+        #sliders[pind].set_val(yvals[pind])
+        fig.canvas.draw_idle()
+
+    fig.canvas.mpl_connect('button_press_event', button_press_callback)
+    fig.canvas.mpl_connect('button_release_event', button_release_callback)
+    fig.canvas.mpl_connect('motion_notify_event', motion_notify_callback)
+
+    #finally show plot
+    plt.show()
+    return
--- multi_res.py
+++ multi_res.py
@@ -3,15 +3,18 @@ from matplotlib.widgets import Slider
 from matplotlib.widgets import RadioButtons
 import numpy as np
 import math
-
+import MRAGUI
 
 FUNCTION_DEPTH=50
 CP_DEPTH=5
 
+curve_points_array=np.array([])
+cur_points=np.array([])
+cur_details=np.array([])
+cur_level=0
+
 def main():
-    #P,Q=get_synthesis_matrices_linear_b_splines(3)
-    #print(P)
-    #print(Q)
+    global curve_points_array, cur_points, cur_details
     curve_points_array = [[0.12043010752688174, 0.7251082251082251],
                             [0.25806451612903225, 0.8777056277056279],
                             [0.42258064516129035, 0.5876623376623378],
@@ -24,30 +27,133 @@ def main():
                             [0.48602150537634414, 0.22077922077922082],
                             [0.1870967741935484, 0.04112554112554115]]
 
-    curve_points_array = [[0.09354838709677418, 0.7142857142857143],
-                          [0.16989247311827957, 0.8041125541125542],
-                          [0.3182795698924731, 0.9253246753246753],
-                          [0.3344086021505377, 0.7716450216450217],
-                          [0.36989247311827955, 0.6147186147186148],
-                          [0.5344086021505376, 0.6017316017316018],
-                          [0.7107526881720431, 0.5963203463203464],
-                          [0.6193548387096774, 0.6872294372294373],
-                          [0.5494623655913978, 0.8452380952380953],
-                          [0.7645161290322581, 0.8484848484848485],
-                          [0.8860215053763442, 0.8257575757575758],
-                          [0.8440860215053764, 0.8041125541125542],
-                          [0.7967741935483871, 0.7456709956709957],
-                          [0.8612903225806451, 0.6201298701298702],
-                          [0.9397849462365592, 0.48484848484848486],
-                          [0.8591397849462367, 0.27489177489177496],
-                          [0.7354838709677419, 0.07034632034632038],
-                          [0.646236559139785, 0.1103896103896104],
-                          [0.49139784946236564, 0.21428571428571433],
-                          [0.3193548387096774, 0.1060606060606061],
-                          [0.1849462365591398, 0.02922077922077926],
-                          [0.11397849462365592, 0.4188311688311689],
-                          [0.1268817204301075, 0.7077922077922079]]
-    all_j_minus_1=calc_new_points(curve_points_array)
+    curve_points_array = np.array(
+        [[0.09354838709677418, 0.7142857142857143],
+         [0.16989247311827957, 0.8041125541125542],
+         [0.3182795698924731, 0.9253246753246753],
+         [0.3344086021505377, 0.7716450216450217],
+         [0.36989247311827955, 0.6147186147186148],
+         [0.5344086021505376, 0.6017316017316018],
+         [0.7107526881720431, 0.5963203463203464],
+         [0.6193548387096774, 0.6872294372294373],
+         [0.5494623655913978, 0.8452380952380953],
+         [0.7645161290322581, 0.8484848484848485],
+         [0.8860215053763442, 0.8257575757575758],
+         [0.8440860215053764, 0.8041125541125542],
+         [0.7967741935483871, 0.7456709956709957],
+         [0.8612903225806451, 0.6201298701298702],
+         [0.9397849462365592, 0.48484848484848486],
+         [0.8591397849462367, 0.27489177489177496],
+         [0.7354838709677419, 0.07034632034632038],
+         [0.646236559139785, 0.1103896103896104],
+         [0.49139784946236564, 0.21428571428571433],
+         [0.3193548387096774, 0.1060606060606061],
+         [0.1849462365591398, 0.02922077922077926],
+         [0.11397849462365592, 0.4188311688311689],
+         [0.1268817204301075, 0.7077922077922079],
+         ])
+
+    curve_points_array=np.array([
+        [0.15698924731182798, 0.31926406926406925],
+        [0.24516129032258063, 0.30519480519480524],
+        [0.17311827956989248, 0.25],
+        [0.1849462365591398, 0.18290043290043292],
+        [0.12258064516129033, 0.1774891774891775],
+        [0.1763440860215054, 0.09632034632034636],
+        [0.2172043010752688, 0.13095238095238101],
+        [0.33225806451612905, 0.04329004329004332],
+        [0.3193548387096774, 0.13419913419913423],
+        [0.42795698924731185, 0.1287878787878788],
+        [0.42365591397849467, 0.2088744588744589],
+        [0.4903225806451613, 0.1677489177489178],
+        [0.546236559139785, 0.26623376623376627],
+        [0.546236559139785, 0.1482683982683983],
+        [0.6193548387096774, 0.16883116883116886],
+        [0.578494623655914, 0.10389610389610393],
+        [0.6913978494623656, 0.08766233766233769],
+        [0.6817204301075269, 0.15692640692640694],
+        [0.789247311827957, 0.15584415584415587],
+        [0.7623655913978495, 0.23376623376623382],
+        [0.8161290322580645, 0.20995670995670998],
+        [0.8172043010752689, 0.27922077922077926],
+        [0.9118279569892472, 0.26515151515151514],
+        [0.9139784946236559, 0.26515151515151514],
+        [0.8913978494623657, 0.2900432900432901],
+        [0.9075268817204302, 0.31493506493506496],
+        [0.8537634408602152, 0.3344155844155845],
+        [0.9376344086021505, 0.4112554112554113],
+        [0.8548387096774194, 0.47186147186147187],
+        [0.921505376344086, 0.49675324675324684],
+        [0.889247311827957, 0.5476190476190477],
+        [0.8924731182795699, 0.6017316017316018],
+        [0.7956989247311829, 0.6244588744588745],
+        [0.8494623655913979, 0.683982683982684],
+        [0.8075268817204302, 0.7402597402597403],
+        [0.8075268817204302, 0.7943722943722944],
+        [0.7494623655913979, 0.7911255411255412],
+        [0.7709677419354839, 0.8430735930735931],
+        [0.6817204301075269, 0.8571428571428572],
+        [0.6645161290322581, 0.8722943722943723],
+        [0.6516129032258065, 0.8409090909090909],
+        [0.5451612903225806, 0.8647186147186148],
+        [0.5817204301075269, 0.8203463203463204],
+        [0.5548387096774194, 0.7727272727272728],
+        [0.6397849462365591, 0.7727272727272728],
+        [0.5903225806451613, 0.7175324675324676],
+        [0.6548387096774194, 0.6872294372294373],
+        [0.6290322580645161, 0.6287878787878789],
+        [0.7075268817204301, 0.5984848484848485],
+        [0.635483870967742, 0.5562770562770564],
+        [0.6333333333333334, 0.6233766233766235],
+        [0.5408602150537635, 0.5627705627705628],
+        [0.5064516129032258, 0.6028138528138529],
+        [0.4688172043010753, 0.5238095238095238],
+        [0.3817204301075269, 0.6114718614718615],
+        [0.45376344086021514, 0.6136363636363636],
+        [0.413978494623656, 0.6547619047619049],
+        [0.32688172043010755, 0.6136363636363636],
+        [0.32688172043010755, 0.6926406926406927],
+        [0.4483870967741935, 0.7218614718614719],
+        [0.3838709677419355, 0.7792207792207793],
+        [0.35376344086021505, 0.7554112554112554],
+        [0.3075268817204301, 0.8181818181818182],
+        [0.346236559139785, 0.8181818181818182],
+        [0.3075268817204301, 0.8766233766233766],
+    ])
+    cur_points=curve_points_array
+
+    MRAGUI.build_plt(curve_points_array, get_new_points_to_draw_for_level)
+    return
+
+def get_new_points_to_draw_for_level(level):
+    global curve_points_array, cur_points, cur_details, cur_level
+    cur_points, cur_details=calc_new_points(cur_points, level)
+    print("return shape: {}, {}".format(cur_points.shape, cur_details.shape))
+
+    return cur_points
+
+def calc_new_points(c_j, level=0):
+    if level==0:
+        j=len(c_j)
+    else:
+        # j=int(len(curve_points_array) / ((level + 1) * 2))
+        j = int(len(c_j) / (2 * level))
+    #print("curve points shape: {}".format(c_j.shape))
+    #print("level: {}, j: {}".format(level, j))
+
+    if level==0:
+        return c_j,np.array([])
+
+    cd_j_minus_1=c_j
+    j = int(len(cd_j_minus_1)/2)
+
+    j=int(math.log(len(cd_j_minus_1)-1,2))
+    #print("new j: {}".format(j))
+
+    print("curve points shape: {}".format(cd_j_minus_1.shape))
+    print("level: {}, j: {}".format(level, j))
+
+    #all_j_minus_1=calc_new_points(curve_points_array)
     #A,B,P,Q, AB=calc_multi_res(int(len(curve_points_array)/2))
 
     #PQ=np.concatenate((P,Q),axis=1)
@@ -58,15 +164,7 @@ def main():
     #d_j_minus_1=np.dot(Q,curve_points_array)
     #all_j_minus_1=all_j_minus_1[:len(A)]
 
-    render(curve_points_array,all_j_minus_1)
-
-
-    exit(0)
-    render()
-    return
-
-def calc_new_points(curve_points_array, step=0):
-    A, B, P, Q, AB = calc_multi_res(int(len(curve_points_array) / ((step+1)*2)))
+    A, B, P, Q, AB = calc_multi_res(j)
     #print(np.shape(A))
     #print(np.shape(B))
     #print(np.shape(P))
@@ -74,175 +172,37 @@ def calc_new_points(curve_points_array, step=0):
 
     # PQ=np.concatenate((P,Q),axis=1)
 
-    all_j_minus_1 = np.dot(AB, curve_points_array)
+    cd_j_minus_1 = np.dot(AB, cd_j_minus_1)
 
     # c_j_minus_1=np.dot(A.T,curve_points_array)
     # d_j_minus_1=np.dot(Q,curve_points_array)
-    all_j_minus_1 = all_j_minus_1[:len(A)]
+    c_j_minus_1 = cd_j_minus_1[:len(A)]
+    d_j_minus_1 = cd_j_minus_1[len(A):]
+    print("c^(j-1) shape: {}".format(c_j_minus_1.shape))
+    print("d^(j-1) shape: {}".format(d_j_minus_1.shape))
 
-    return all_j_minus_1
-
-
-def render_subdivision_step_update(ax, control_points_array, points_array=None, step=None):
-    ax.clear()
-    ax.axis([0.0, 1.0, 0.0, 1.0])
-
-    #radius
-    r_cp=0.0025
-    r_p=0.0025
-    #color
-    c_cp='black'
-    c_p='blue'
-    c_sc='red'
-    c_cc='gray'
-
-    ann_offset = np.array([0.006, 0.003])  # offset for annotations
-
-    if control_points_array is not None:
-        for i, cp in enumerate(control_points_array):
-            disc = "c{}".format(i)
-            ax.add_patch(plt.Circle(cp, radius=r_cp, fc=c_cp, fill=True))
-            ax.annotate(disc, cp + ann_offset)
-
-            if i == 0:
-                from_p = control_points_array[len(control_points_array) - 1]
-            else:
-                from_p = control_points_array[i - 1]
-            to_p = cp
-            ax.plot([from_p[0], to_p[0]], [from_p[1], to_p[1]], 'o', ms=2.0, ls='-', lw=1.0, color=c_cc)
-
-    if points_array is not None:
-        for i,p in enumerate(points_array):
-            ax.add_patch(plt.Circle(p, radius=r_p, fc=c_p, fill=False))
-            if i==0:
-                from_p=points_array[len(points_array)-1]
-            else:
-                from_p=points_array[i-1]
-            to_p=p
-            ax.plot([from_p[0], to_p[0]], [from_p[1], to_p[1]], 'o', ms=2.0, ls='-', lw=1.0, color=c_sc)
-    return
-
-def render(control_points_array, points_array):
-    #prepare plot
-    fig, ax = plt.subplots(figsize=[12, 12])
-    #init vis
-    cur_step=0
-    #points_array = calc_subdivision(control_points_array, cur_step)
-    #render_subdivision_step_update(control_points_array, points_array, cur_step, ax)
-    render_subdivision_step_update(ax, control_points_array, points_array, cur_step)
-    #Slider (widget example adapted from: https://riptutorial.com/matplotlib/example/23577/interactive-controls-with-matplotlib-widgets)
-    #slider axes
-    slider_ax = plt.axes([0.35, .03, 0.50, 0.02])
-    subdivision_slider = Slider(slider_ax, "Level", 0, 10, valinit=cur_step, valstep=1)
-
-    #defined locally to have all values here
-    def update_vis(step):
-        #get selected step
-        nonlocal cur_step
-        cur_step=step
-
-        points_array=calc_new_points(control_points_array, cur_step)
+    return c_j_minus_1, d_j_minus_1
 
 
-        #count_cp=len(control_points_array)
-        #count_p=count_cp*math.pow(2,step)#+step
-        #print("Replot at Level {} on {} control points with {} total points".format(step, count_cp, count_p))
 
-        #rebuild vis on axes
-        render_subdivision_step_update(ax, control_points_array, points_array)
-        fig.canvas.draw_idle()
-
-    # call update function on slider value change
-    subdivision_slider.on_changed(update_vis)
-
-    # this was to define points for initial curve ;)
-    def onclick(event):
-        ix, iy = event.xdata, event.ydata
-        print("[{}, {}],".format(ix,iy))
-    fig.canvas.mpl_connect('button_press_event', onclick)
-
-    #finally show plot
-    plt.show()
-    return
-
-def splitting_step(points_array):
-    points_array=np.repeat(points_array,2,axis=0)
-    # each point is now doubled
-    cp_points_array = np.copy(points_array)
-    for i, p in enumerate(points_array):
-        if i%2==1:
-            next_p = points_array[i + 1] if i<len(points_array)-1 else points_array[0]
-            cp_points_array[i]=0.5*(p+next_p)
-    return cp_points_array
-
-def splitting_step_nonuniform(points_array):
-    points_array=np.repeat(points_array,2,axis=0)
-    # each point is now doubled
-    cp_points_array = np.copy(points_array)
-    for i, p in enumerate(points_array):
-        if i<len(points_array)-1:
-            if i%2==1:
-                next_p = points_array[i + 1]
-                cp_points_array[i]=0.5*(p+next_p)
-    #pop last (doubled) element
-    return cp_points_array[:-1]
-
-def subdivision_b_spline_cubic_nonuniform(points_array):
-    cp_points_array=points_array
-    c_size=len(cp_points_array)
-
-    if c_size<8:
-        print("too few values in point_array. Abort subdivision")
-        return points_array
-
-    #build up averaging matrix
-    mat_r=np.zeros((c_size, c_size))
-    for i in range(3,c_size-3):
-        mat_r[i,i]=2
-        if i>0: mat_r[i,i-1]=1
-        if i<c_size-1: mat_r[i,i+1]=1
-    mat_r[0,0]=4
-    mat_r[c_size-1,c_size-1]=4
-
-    mat_r[1,1]=4
-    mat_r[c_size-2,c_size-2]=4
-
-    mat_r[2,2]=2
-    mat_r[2,1]=2
-
-    mat_r[c_size-3,c_size-3]=2
-    mat_r[c_size-3,c_size-2]=2
-
-    mat_r[3,2]=3.0/2
-    mat_r[3,3]=3.0/2
-    mat_r[c_size-4,c_size-3]=3.0/2
-    mat_r[c_size-4,c_size-4]=3.0/2
-
-    mat_r*=1.0/4
-
-    #print(mat_r)
-    points_array[:,1]=np.dot(mat_r,cp_points_array[:,1])
-
-    #for i in range(0,c_size):
-        #if i%2==1:
-            #points_array[:,1]=cp_points_array[:,1]
-    #print(points_array)
-
-    return points_array
 
 def get_synthesis_matrices_linear_b_splines(j):
 
     norm=True
     #calc mat-sizes
-    cols_of_P=j+1
+    cols_of_P=2**(j-1)+1
+
     #shifted down by 2 (for linear) plus endpoint
-    rows_of_P=2*j+1
+    #rows_of_P=2*j+1
+    rows_of_P=2**j+1
     #quadratic
     rows_of_Q=rows_of_P
     cols_of_Q=rows_of_P-cols_of_P
 
     P = np.zeros((rows_of_P, cols_of_P))
     Q = np.zeros((rows_of_Q, cols_of_Q))
+
+    print("shape P: {}, Q: {}".format(P.shape, Q.shape))
 
     #P is always the same
     for col in range(0, cols_of_P):
@@ -281,6 +241,11 @@ def get_synthesis_matrices_linear_b_splines(j):
             Q[col*2-1+2, col] = 10
             Q[col*2-1+3, col] = -6
             Q[col*2-1+4, col] = 1
+            #Q[col*2-1, col] = 1
+            #Q[col*2-1+1, col] = -6
+            #Q[col*2-1+2, col] = 10
+            #Q[col*2-1+3, col] = -6
+            #Q[col*2-1+4, col] = 1
 
         #fix values
         Q[0,0]=-11.022704
@@ -305,6 +270,7 @@ def calc_multi_res(j):
 
     #sythesis
     P,Q=get_synthesis_matrices_linear_b_splines(j)
+    print("shapes: P: {}, Q: {}".format(P.shape, Q.shape))
     #analysis
     #only for orthogonal
         #A=P.T
@@ -314,12 +280,12 @@ def calc_multi_res(j):
     #print(PQ)
     #\frac{A,B}=P|Q^-1
     AB=np.linalg.inv(PQ)
-    print(AB)
+    #print(AB)
 
     #split up AB
-    A,B=np.vsplit(AB,[j+1])
-    print(A)
-    print(B)
+    A,B=np.vsplit(AB,[2**(j-1)+1])
+    #print(A)
+    #print(B)
 
     return A,B,P,Q, AB
     #analysis (decomposotion)
@@ -345,19 +311,6 @@ def calc_multi_res(j):
 
     return
 
-def calc_subdivision(control_points_array, steps):
-    points_array=control_points_array
-    for i in range(0,steps):
-        #SPLITTING STEP
-        points_array=splitting_step_nonuniform(points_array)
-        #print(points_array)
-        #print(points_array.size)
-
-        # AVG STEP
-        #points_array=subdivision_scheme_function(points_array)
-        #points_array=subdivision_b_spline_cubic_nonuniform(points_array)
-
-    return points_array
 
 if __name__ == "__main__":
     # execute only if run as a script
